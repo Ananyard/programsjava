5ac263f26a2e55c55e2fc59718a716b10125b17c
Ananyard <ananyard21@gmail.com>
2025-08-22 08:57:28 +0530

    file

---
+++ PalindromeCheck.java
@@ -0,0 +1,18 @@
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int temp = n, rev = 0;
+
+        while (n > 0) {
+            rev = rev * 10 + (n % 10);
+            n /= 10;
+        }
+
+        if (temp == rev)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a palindrome");
+    }
+}
