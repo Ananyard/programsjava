d842db1cee5ac46167f72d19599d83de0d770e65
Ananyard <109667734+Ananyard@users.noreply.github.com>
2025-08-26 19:29:07 +0530

    Update SumEvenOddDigits.java

--- SumEvenOddDigits.java
+++ SumEvenOddDigits.java
@@ -1,4 +1,7 @@
+
+import java.util.Scanner;
 public class SumEvenOddDigits {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
